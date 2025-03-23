8192f68a5a3001ee6ca072c92f5feba59754ea2f
SayeedSanjana <sanjanasayeed68@gmail.com>
2025-03-23 17:36:57 -0400

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <>Trendify</>;
+  return <Button variant="default">Trendify</Button>;
 };
 export default HomePage;
