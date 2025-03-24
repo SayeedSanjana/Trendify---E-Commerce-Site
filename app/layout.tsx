984ceecb52a7ffed2f630ffc14eb1d237d2219cc
SayeedSanjana <sanjanasayeed68@gmail.com>
2025-03-24 01:20:28 -0400

    Root layout changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "../lib/constants";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Trendify",
-  description: "A ecommerce site using Next.js",
+  title: {
+    template: `%s | Trendify`,
+    default: APP_NAME,
+  },
+  description: `${APP_DESCRIPTION}`,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
